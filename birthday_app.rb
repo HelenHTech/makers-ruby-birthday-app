d8fe6c11025ec6db683aa76d7bb757652b5839e5
Helen Hayward <helen.m.hayward@hotmail.co.uk>
2019-05-30 17:25:58 +0100

    If statement added to ensure Happy Birthday name is printed

--- birthday_app.rb
+++ birthday_app.rb
@@ -2,13 +2,12 @@ require 'sinatra'
 set :session_secret, 'super secret'
 
 get '/whenisyourbirthday' do
-  @date_today = (Date.today).strftime("%Y-%m-%d")
-  p @date_today
   erb :whenisyourbirthday
 end
 
 post '/birthdayanswer' do
-  p @name = params[:name]
-  p @date = params[:date]
+  @date_today = (Date.today).strftime("%Y-%m-%d")
+  @name = params[:name]
+  @date = params[:date]
   erb :birthdayanswer
 end
